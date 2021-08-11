8fe42d10c8ec9fc1be1dc10a4650862b6d56ae3a
HyojeongRyu <ryu8701@gmail.com>
2021-08-11 19:51:35 +0900

    test

---
+++ 1stWeek/square_star.js
@@ -0,0 +1,13 @@
+//이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+//별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+//[제한조건]
+//  n과 m은 각각 1000 이하인 자연수입니다.
+
+const n=document.getElementById('n').value;
+const m=document.getElementById('m').value;
+const draw=document.getElementById('draw');
+const res=document.getElementById('res');
+draw.onclick=()=>{
+    res.value="*"
+}
+
