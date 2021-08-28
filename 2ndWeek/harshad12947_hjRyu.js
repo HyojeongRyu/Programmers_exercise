ec42fe2386b858f59812faa0875f8e09084c66b4
HyojeongRyu <ryu8701@gmail.com>
2021-08-29 04:16:02 +0900

    harshad12947_hjRyu 예외처리 추가

--- 2ndWeek/harshad12947_hjRyu.js
+++ 2ndWeek/harshad12947_hjRyu.js
@@ -4,6 +4,10 @@ const btn=document.getElementById('btn')
 btn.addEventListener('click',()=>{
     const x = document.getElementById('x').value
     const length=x.length
+    if(Number(x)>10000||Number(x)<1){
+        alert('x는 1 이상, 10000 이하인 정수여야합니다')
+        return;
+    }
     let sum=0;
     for(let i=0; i<length; i++){
         sum+=Number(x[i])
