a7ea43bd9a493d4463bbc841152bf5c4966545f8
HyojeongRyu <ryu8701@gmail.com>
2021-11-22 22:46:48 +0900

    12Week

--- 12ndWeek/minSquare86491.js
+++ 12ndWeek/minSquare86491.js
@@ -2,16 +2,6 @@
 //2차원 배열 sizes가 매개변수로 주어집니다.
 //모든 명함을 수납할 수 있는 가장 작은 지갑을 만들 때,
 //지갑의 크기를 return 하도록 solution 함수를 완성해주세요.
-// function solution(sizes) {
-//     const width=[];
-//     const height=[];
-//     sizes.map(x=>{
-//         x.sort((a,b)=>a-b)
-//         width.push(x[0])
-//         height.push(x[1])
-//     })
-//     return Math.max(...width)*Math.max(...height)
-// }
 
 function solution(sizes) {
     let maxW=0;
--- 12ndWeek/sumTwo68644.js
+++ 12ndWeek/sumTwo68644.js
@@ -1,15 +1,31 @@
+//정수 배열 numbers가 주어집니다.
+//numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아
+//더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아
+//return 하도록 solution 함수를 완성해주세요.
+
 function solution(numbers) {
     var answer = [];
     for (let i = 0; i < numbers.length; i++) {
         for (let j = i+1; j < numbers.length; j++) {
-            answer.push(numbers[i]+numbers[j])
+            if(!answer.includes(numbers[i]+numbers[j])){
+                answer.push(numbers[i]+numbers[j])
+            }
         }
-       
     }
     answer.sort((a,b)=>{return a-b})
-    for (let i = 0; i < answer.length; i++) {
-        if(answer[i]==answer[i+1]||answer[i]==answer[i-1]){answer.splice(i,1)}
-    }
     return answer;
 }
-console.log(solution([5,0,2,7]))
+
+//function solution(numbers) {
+//     const temp = []
+
+//     for (let i = 0; i < numbers.length; i++) {
+//         for (let j = i + 1; j < numbers.length; j++) {
+//             temp.push(numbers[i] + numbers[j])
+//         }
+//     }
+
+//     const answer = [...new Set(temp)]
+
+//     return answer.sort((a, b) => a - b)
+// }
