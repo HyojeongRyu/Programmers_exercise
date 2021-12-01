ad322b6e065fedd7ed35b8ec91db303638febe82
HyojeongRyu <ryu8701@gmail.com>
2021-12-02 00:06:15 +0900

    14Week 풀이중

---
+++ 14thWeek/failRate42889.js
@@ -0,0 +1,13 @@
+//전체 스테이지의 개수 N,
+//게임을 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열 stages
+//실패율이 높은 스테이지부터 내림차순으로
+//스테이지의 번호가 담겨있는 배열을 return
+
+function solution(N, stages) {
+    
+    
+}
+
+const N=5;
+const stages=[2,1,2,6,2,4,3,3]
+console.log(solution(N,stages))
