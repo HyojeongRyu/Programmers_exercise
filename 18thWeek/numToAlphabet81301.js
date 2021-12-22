e9c2e08e221768f44d5cff345218af511dcd0ca0
HyojeongRyu <ryu8701@gmail.com>
2021-12-23 00:38:47 +0900

    18thWeek new ID

---
+++ 18thWeek/numToAlphabet81301.js
@@ -0,0 +1,18 @@
+function solution(s) {
+    var table=[
+        {'0':'zero'},
+        {'1':'one'},
+        {'2':'two'},
+        {'3':'three'},
+        {'4':'four'},
+        {'5':'five'},
+        {'6':'six'},
+        {'7':'seven'},
+        {'8':'eight'},
+        {'9':'nine'}
+    ]
+}
+
+vars="one4seveneight"
+table.map()
+console.log(solution(s))
